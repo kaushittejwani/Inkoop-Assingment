1585045aa60beac9e10162da5f9d910966283bb5
kaushittejwani <95282960+kaushittejwani@users.noreply.github.com>
2023-01-04 10:22:29 +0530

    add array push metbod

--- App.js
+++ App.js
@@ -24,9 +24,11 @@ function App() {
       for(i=0;i<=2;i++){
         
         if(i==0){
+          //array methods
           arr.push(+num+2)
         }
         else{
+          //array methods
           arr.push(+arr[i-1]+2)
         }
       }
@@ -37,7 +39,7 @@ function App() {
       }
       
     
-  //array methods
+ 
   
     
   
